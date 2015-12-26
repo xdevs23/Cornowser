bdcc4d4c53de0ccb18c82bb3156c736c07b2d430
xdevs23 <00simy@gmail.com>
2015-12-26 11:48:01 +0100

    First commit

---
+++ app/src/main/java/io/xdevs23/cornowser/browser/CornBrowser.java
@@ -0,0 +1,13 @@
+package io.xdevs23.cornowser.browser;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CornBrowser extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_corn);
+    }
+}
