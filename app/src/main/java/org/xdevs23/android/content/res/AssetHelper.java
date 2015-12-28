61f67a9401ddd0f32e400ff0a727881b8676c044
xdevs23 <00simy@gmail.com>
2015-12-28 18:51:39 +0100

    Added HttpStatusCodeHelper \(GREAT\!\)

---
+++ app/src/main/java/org/xdevs23/android/content/res/AssetHelper.java
@@ -0,0 +1,7 @@
+package org.xdevs23.android.content.res;
+
+/**
+ * Created by simao on 28.12.15.
+ */
+public class AssetHelper {
+}
---
+++ app/src/main/java/org/xdevs23/io/stream/InputStreamUtils.java
@@ -0,0 +1,7 @@
+package org.xdevs23.io.stream;
+
+/**
+ * Created by simao on 28.12.15.
+ */
+public class InputStreamUtils {
+}
