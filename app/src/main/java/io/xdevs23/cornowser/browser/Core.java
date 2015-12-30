f6e673a16d0563c0de1a4d3d572b576a888ea35c
xdevs23 <00simy@gmail.com>
2015-12-30 10:42:11 +0100

    Temporarily switched to Google Analytics

--- app/src/main/java/io/xdevs23/cornowser/browser/Core.java
+++ app/src/main/java/io/xdevs23/cornowser/browser/Core.java
@@ -2,11 +2,7 @@ package io.xdevs23.cornowser.browser;
 
 import android.app.Application;
 
-import com.crashlytics.android.Crashlytics;
-
 import org.xdevs23.debugutils.Logging;
-
-import io.fabric.sdk.android.Fabric;
 
 public class Core extends Application {
 
@@ -14,9 +10,6 @@ public class Core extends Application {
     public void onCreate() {
         Logging.logd("PRE INIT START");
         super.onCreate();
-
-        Logging.logd("Initializing Crashlytics");
-        Fabric.with(this, new Crashlytics());
     }
 
 }
