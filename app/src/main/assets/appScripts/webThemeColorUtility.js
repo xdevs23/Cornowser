e90c62722f6124a0a706d9454fa0eb5c79bd5d60
xdevs23 <xdevs23@outlook.com>
2016-02-18 16:20:29 +0100

    Optimized omnibox tinting

--- app/src/main/assets/appScripts/webThemeColorUtility.js
+++ app/src/main/assets/appScripts/webThemeColorUtility.js
@@ -3,16 +3,16 @@ try {
     webCol = document.querySelector("meta[name='theme-color']").getAttribute("content").toString();
 } catch(a) {
     try {
-        webCol = document.querySelector("meta[name='apple-mobile-web-app-status-bar-style']").getAttribute("content").toString();
+        webCol = document.querySelector("meta[name='msapplication-TileColor']").getAttribute("content").toString();
     } catch(b) {
         try {
-            webCol = document.querySelector("meta[name='msapplication-navbutton-color']").getAttribute("content").toString();
+            webCol = document.querySelector("meta[name='web-color']").getAttribute("content").toString();
         } catch(c) {
             try {
-                webCol = document.querySelector("meta[name='web-color']").getAttribute("content").toString();
+                webCol = document.querySelector("meta[name='apple-mobile-web-app-status-bar-style']").getAttribute("content").toString();
             } catch(d) {
                 try {
-                    webCol = document.querySelector("meta[name='msapplication-TileColor']").getAttribute("content").toString();
+                    webCol = document.querySelector("meta[name='msapplication-navbutton-color']").getAttribute("content").toString();
                 } catch(e) {
                     webCol = "default";
                 }
@@ -25,10 +25,11 @@ try {
     if(webCol === "default") {
         var cu = document.location.host;
 
-        if     (cu.indexOf("facebook")  !== -1) webCol = "#3b5998";
-        else if(cu.indexOf("google")    !== -1) webCol = "#484848";
-        else if(cu.indexOf("twitter")   !== -1) webCol = "#55acee";
-        else if(cu.indexOf("youtube")   !== -1) webCol = "#dd0804";
+        if     (cu.indexOf("facebook")          !== -1) webCol = "#3b5998";
+        else if(cu.indexOf("google")            !== -1) webCol = "#484848";
+        else if(cu.indexOf("twitter")           !== -1) webCol = "#55acee";
+        else if(cu.indexOf("youtube")           !== -1) webCol = "#dd0804";
+        else if(cu.indexOf("xda-developers")    !== -1) webCol = "#de7300";
 
         else webCol = "default";
 
@@ -37,26 +38,8 @@ try {
     webCol = "default";
 }
 
-function tryElem(elem) {
-    if(webCol !== "default") return webCol;
-    try {
-        return document.getElementById(elem).style.backgroundColor;
-    } catch(ex) {
-        return "default";
-    }
-}
+if(webCol === "") webCol = "default";
 
-try {
-    if(webCol === "default") {
-        webCol = tryElem("navbar");
-        webCol = tryElem("titlebar");
-        webCol = tryElem("navigation");
-    }
-    if(webCol === "") webCol = "default";
-    if(webCol.toUpperCase() === "#FFFFFF") webCol = "default";
-} catch(ex) {
-    webCol = "default";
-}
 
 console.log("CornHandler://setWebThemeColor:" + webCol);
 
