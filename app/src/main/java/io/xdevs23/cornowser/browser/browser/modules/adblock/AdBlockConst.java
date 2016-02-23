4c3bd0c7314334b7c2817e17ee8c22920e8022ac
xdevs23 <xdevs23@outlook.com>
2016-02-23 16:12:23 +0100

    Started ad blocker

---
+++ app/src/main/java/io/xdevs23/cornowser/browser/browser/modules/adblock/AdBlockConst.java
@@ -0,0 +1,13 @@
+package io.xdevs23.cornowser.browser.browser.modules.adblock;
+
+
+public final class AdBlockConst {
+
+    public static final String
+        URL_ADAWAY_LIST = "http://adaway.org/hosts.txt",
+        URL_MVPS_LIST   = "http://winhelp2002.mvps.org/hosts.txt",
+        URL_HOST_F_LIST = "http://hosts-file.net/ad_servers.txt",
+        URL_YOYO_LIST   = "https://pgl.yoyo.org/adservers/serverlist.php?hostformat=hosts&showintro=0&mimetype=plaintext"
+            ;
+
+}
---
+++ app/src/main/java/io/xdevs23/cornowser/browser/browser/modules/adblock/AdBlockManager.java
@@ -0,0 +1,16 @@
+package io.xdevs23.cornowser.browser.browser.modules.adblock;
+
+public class AdBlockManager {
+
+    protected static String[] hosts;
+
+
+    private AdBlockManager() {
+
+    }
+
+    public static void loadHosts() {
+        
+    }
+
+}
