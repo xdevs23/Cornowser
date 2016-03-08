e89a512f16e204ec4ccbaad883a98b51a3ce68a5
xdevs23 <xdevs23@outlook.com>
2016-03-08 18:48:20 +0100

    Update crosswalk version to 19.48.495.0

--- configure.sh
+++ configure.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
+function welcome() {
+  echo "This is the configuration script to start building Cornowser."
+  echo "Please make sure to have a private key created in order to proceed."
+  echo "If you don't have one,  create one using i. e. Android Studio"
+  echo "Then come back to this script and enter your credentials"
+  echo "Note: The keystore must be saved as privatekey.jks"
+}
+
+function goodbye() {
+  echo "Congratz! Seems that the configuration process was successful."
+}
+
+welcome
+
+echo "First of all the keystore needs to be configured."
+
 if [ "$1" == "--clearPw" ]; then
   export ENV_CORNOWSER_KEYPWD=""
   export ENV_CORNOWSER_STOREPWD=""
@@ -14,16 +30,20 @@ if [ "$1" == "--gpw" ]; then
 fi
 
 if [ -z $ENV_CORNOWSER_KEYPWD ]; then
-  echo "This is the configuration script to start building Cornowser."
-  echo "Please make sure to have a private key created in order to proceed."
-  echo "If you don't have one,  create one using i. e. Android Studio"
-  echo "Then come back to this script and enter your credentials"
-  echo "Note: The keystore must be saved as privatekey.jks"
   echo -en "\nKeystore password: "
   read -s -p "" CORNOWSER_STOREPWD
   echo -en "\nKey      password: "
   read -s -p "" CORNOWSER_KEYPWD
   export ENV_CORNOWSER_KEYPWD="$CORNOWSER_KEYPWD"
   export ENV_CORNOWSER_STOREPWD="$CORNOWSER_STOREPWD"
-  echo "Congratz! Seems that the configuration process was successful."
+  echo -en "\n"
 fi
+
+echo -en "\n"
+
+echo "Configuring XWalk..."
+cd crosswalk
+bash xwalkinstaller.sh auto --nominify
+cd ../
+
+goodbye
