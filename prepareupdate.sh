d36c662c82930347d707828fa82054c5f570892d
xdevs23 <xdevs23@outlook.com>
2016-03-15 17:46:52 +0100

    Fix prepare update script

--- prepareupdate.sh
+++ prepareupdate.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+APPRELPATH="app/build/outputs/apk/app-release-signed.apk"
+
 if [ -z $1 ]; then echo "Usage: $0 vername vercode vertype"
 else
 source make.sh
 
-cp app/app-release.apk update/Cornowser.apk
-cp "app/app-release.apk" "versions/Cornowser-$1-$2$3.apk"
+cp "$APPRELPATH" "update/Cornowser.apk"
+cp "$APPRELPATH" "versions/Cornowser-$1-$2$3.apk"
 
 echo -en $1>update/version.txt
 echo -en $2>update/rel.txt
