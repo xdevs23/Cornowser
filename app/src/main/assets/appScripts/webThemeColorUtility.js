70ebd528d5e0339e79b41ea5fdc41ae76d10e6b6
xdevs23 <xdevs23@outlook.com>
2016-03-15 16:55:56 +0100

    Omnibox tinting: add web pages

--- app/src/main/assets/appScripts/webThemeColorUtility.js
+++ app/src/main/assets/appScripts/webThemeColorUtility.js
@@ -26,6 +26,8 @@ try {
         var cu = document.location.host;
 
         if     (cu.indexOf("facebook")          !== -1) webCol = "#3b5998";
+        else if(cu.indexOf("plus.google")       !== -1) webCol = "#d32f2f";
+        else if(cu.indexOf("play.google")       !== -1) webCol = "#60c416";
         else if(cu.indexOf("google")            !== -1) webCol = "#484848";
         else if(cu.indexOf("twitter")           !== -1) webCol = "#55acee";
         else if(cu.indexOf("youtube")           !== -1) webCol = "#dd0804";
@@ -35,6 +37,7 @@ try {
         else if(cu.indexOf("stackoverflow")     !== -1) webCol = "#23334e";
         else if(cu.indexOf("androidfilehost")   !== -1) webCol = "#222222";
         else if(cu.indexOf("codepen")           !== -1) webCol = "#1d1f20";
+        else if(cu.indexOf("jsfiddle")          !== -1) webCol = "#323232";
         else if(cu.indexOf("agar")              !== -1) webCol = "#428bca";
 
         else webCol = "default";
