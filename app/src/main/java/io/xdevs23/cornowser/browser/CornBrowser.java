c16ff9f744fbbff0902c8a681936018258d01fd0
xdevs23 <xdevs23@outlook.com>
2016-03-25 13:50:15 +0100

    Fix crash No. 20

--- app/src/main/java/io/xdevs23/cornowser/browser/CornBrowser.java
+++ app/src/main/java/io/xdevs23/cornowser/browser/CornBrowser.java
@@ -909,14 +909,14 @@ public class CornBrowser extends XquidCompatActivity {
                     || intent.getStringExtra(BgLoadActivity.bgLoadKey).isEmpty())
                 ) return;
         Logging.logd("URL information in intent found");
-        if( intent.getStringExtra(URL_TO_LOAD_KEY) != null
-                || intent.getStringExtra(URL_TO_LOAD_KEY).isEmpty() ) {
+        if(! (intent.getStringExtra(URL_TO_LOAD_KEY) == null
+                || intent.getStringExtra(URL_TO_LOAD_KEY).isEmpty()) ) {
             getIntent().putExtra(URL_TO_LOAD_KEY, intent.getStringExtra(URL_TO_LOAD_KEY));
             handleStartupWebLoad();
             return;
         }
-        if( intent.getStringExtra(BgLoadActivity.bgLoadKey) != null ||
-                intent.getStringExtra(BgLoadActivity.bgLoadKey).isEmpty() ) {
+        if(! (intent.getStringExtra(BgLoadActivity.bgLoadKey) == null ||
+                intent.getStringExtra(BgLoadActivity.bgLoadKey).isEmpty()) ) {
             getIntent().putExtra(BgLoadActivity.bgLoadKey,
                     intent.getStringExtra(BgLoadActivity.bgLoadKey));
             isBgBoot = true;
