c63dea7ea1fcec0c560779b9c580fa6046f60601
xdevs23 <xdevs23@outlook.com>
2016-04-13 18:24:10 +0200

    configure.sh: fix typo

--- configure.sh
+++ configure.sh
@@ -3,7 +3,7 @@
 function welcome() {
   echo "This is the configuration script to start building Cornowser."
   echo "Please make sure to have a private key created in order to proceed."
-  echo "If you don't have one,  create one using i. e. Android Studio"
+  echo "If you don't have one, create one using i. e. Android Studio"
   echo "Then come back to this script and enter your credentials"
   echo "Note: The keystore must be saved as privatekey.jks"
 }
