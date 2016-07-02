b4f00d3f826cd85d9e59bc5fadd7ac99e85cba77
xdevs23 <xdevs23@outlook.com>
2016-07-02 15:52:05 +0200

    XDListView: add listview with 2 rows

--- app/src/main/java/org/xdevs23/ui/view/listview/XDListView.java
+++ app/src/main/java/org/xdevs23/ui/view/listview/XDListView.java
@@ -1,7 +1,17 @@
 package org.xdevs23.ui.view.listview;
 
 import android.content.Context;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import org.xdevs23.debugutils.Logging;
+
+import java.util.ArrayList;
 
 import io.xdevs23.cornowser.browser.R;
 
@@ -16,6 +26,50 @@ public class XDListView {
 
     public static ArrayAdapter<String> createLittle(Context context, String[] content) {
         return new ArrayAdapter<String>(context, R.layout.listview_little, content);
+    }
+
+    public static ArrayAdapter<String> createDual(Context context, String[] first, String[] second) {
+        String[] all = new String[first.length * 2];
+        for ( int i = 0; i < first.length; i++ ) {
+            all[i]                =  first[i];
+            all[first.length + i] = second[i];
+        }
+        return new DualAdapter(context, R.layout.listview_activity_dualrow, all);
+    }
+
+    public static class DualAdapter extends ArrayAdapter<String> {
+
+        private int allLength = 0;
+
+        protected static class ViewHolder {
+            public TextView TextView1, TextView2;
+        }
+
+        public int getCount() {
+            return allLength / 2;
+        }
+
+        public long getItemId(int position) {
+            return position;
+        }
+
+        public DualAdapter(Context context, int resource, String[] content) {
+            super(context, resource, content);
+            allLength = content.length;
+        }
+
+        public View getView(int position, View convertView, ViewGroup parent) {
+            ViewHolder holder = new ViewHolder();
+            LayoutInflater inflater = LayoutInflater.from(getContext());
+            RelativeLayout mainView = (RelativeLayout)
+                    inflater.inflate(R.layout.listview_activity_dualrow, null);
+            holder.TextView1 = (TextView) mainView.getChildAt(0);
+            holder.TextView2 = (TextView) mainView.getChildAt(1);
+            holder.TextView1.setText(getItem(position));
+            holder.TextView2.setText(getItem(allLength / 2 + position));
+            convertView = mainView;
+            return convertView;
+        }
     }
 	
 }
